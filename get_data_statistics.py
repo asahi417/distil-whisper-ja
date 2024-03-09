faa3f855e174e96de547df5cb727d5cc1c122990
asahi417 <asahi1992ushio@gmail.com>
2024-03-09 14:29:18 +0900

    add eval dataset

--- get_data_statistics.py
+++ get_data_statistics.py
@@ -3,11 +3,14 @@ import os.path
 
 from datasets import load_dataset
 from tqdm import tqdm
+from transformers import WhisperTokenizer
+
+TOKENIZER = WhisperTokenizer.from_pretrained("openai/whisper-large-v3", language="japanese")
 
 
 def get_cumulative_max_min(
-        current_value: float,
         cumulative_value: float,
+        current_value: float,
         current_max: float,
         current_min: float,
 ):
@@ -35,8 +38,7 @@ def dataset_statistics(data: str = "reazonspeech", data_type: str = "tiny"):
         )
     elif data == "common_voice_8_0":
         dataset = load_dataset(
-            "mozilla-foundation/common_voice_8_0",
-            "ja",
+            "asahi417/ja_asr.common_voice_8_0",
             split="test",
             trust_remote_code=True
         )
@@ -47,45 +49,67 @@ def dataset_statistics(data: str = "reazonspeech", data_type: str = "tiny"):
     amp_max, amp_max_max, amp_max_min = 0, None, None
     amp_min, amp_min_max, amp_min_min = 0, None, None
     amp_mean, amp_mean_max, amp_mean_min = 0, None, None
+    text, text_max, text_min = 0, None, None
+    token, token_max, token_min = 0, None, None
     data_size = 0
     for value in tqdm(iterator):
         ar = value['audio']['array']
-        duration, duration_max, duration_min = get_cumulative_max_min(duration, len(ar) / value['audio']['sampling_rate'], duration_max, duration_min)
+        transcription_char_size = len(value["transcription"])
+        transcription_token_size = len(TOKENIZER(value["transcription"])["input_ids"])
+
+        duration, duration_max, duration_min = get_cumulative_max_min(
+            duration,
+            len(ar) / value['audio']['sampling_rate'],
+            duration_max,
+            duration_min
+        )
         amp_max, amp_max_max, amp_max_min = get_cumulative_max_min(amp_max, ar.max(), amp_max_max, amp_max_min)
         amp_min, amp_min_max, amp_min_min = get_cumulative_max_min(amp_min, ar.min(), amp_min_max, amp_min_min)
         amp_mean, amp_mean_max, amp_mean_min = get_cumulative_max_min(amp_mean, ar.mean(), amp_mean_max, amp_mean_min)
+        text, text_max, text_min = get_cumulative_max_min(text, transcription_char_size, text_max, text_min)
+        token, token_max, token_min = get_cumulative_max_min(token, transcription_token_size, token_max, token_min)
         data_size += 1
     return {
-        "duration": [duration/data_size, duration_max, duration_min],
-        "amp_max": [amp_max / data_size, amp_max_max, amp_max_min],
-        "amp_min": [amp_min / data_size, amp_min_max, amp_min_min],
-        "amp_mean": [amp_mean / data_size, amp_mean_max, amp_mean_min],
-        "data_size": data_size
+        "audio": {
+            "duration": {"mean": duration / data_size, "max": duration_max, "min": duration_min},
+            "amplitude_max": {"mean": amp_max / data_size, "max": amp_max_max, "min": amp_max_min},
+            "amplitude_min": {"mean": amp_min / data_size, "max": amp_min_max, "min": amp_min_min},
+            "amplitude_mean": {"mean": amp_mean / data_size, "max": amp_mean_max, "min": amp_mean_min},
+        },
+        "transcription": {
+            "character_length": {"mean": text / data_size, "max": text_max, "min": text_min},
+            "token_length": {"mean": token / data_size, "max": token_max, "min": token_min},
+        },
+        "total_data_size": data_size,
+        "total_audio_duration (sec)": duration,
+        "total_transcription_size (character)": text
     }
 
-if os.path.exists("data_statistics.json"):
-    with open("data_statistics.json") as f:
-        stats = json.load(f)
-else:
-    stats = {}
 
-if "ja_asr.jsut-basic5000" not in stats:
-    stats["ja_asr.jsut-basic5000"] = dataset_statistics("ja_asr.jsut-basic5000")
-if "common_voice_8_0" not in stats:
-    stats["common_voice_8_0"] = dataset_statistics("common_voice_8_0")
-if "reazonspeech.tiny" not in stats:
-    stats["reazonspeech.tiny"] = dataset_statistics(data_type="tiny")
-if "reazonspeech.small" not in stats:
-    stats["reazonspeech.small"] = dataset_statistics(data_type="small")
-if "reazonspeech.medium" not in stats:
-    stats["reazonspeech.medium"] = dataset_statistics(data_type="medium")
-# if "reazonspeech.large" in stats:
-#     stats["reazonspeech.large"] = dataset_statistics(data_type="large")
-# if "reazonspeech.all" in stats:
-#     stats["reazonspeech.all"] = dataset_statistics(data_type="all")
+if __name__ == '__main__':
+    if os.path.exists("data_statistics.json"):
+        with open("data_statistics.json") as f:
+            stats = json.load(f)
+    else:
+        stats = {}
 
-with open("data_statistics.json", "w") as f:
-    json.dump(stats, f)
+    if "ja_asr.jsut-basic5000" not in stats:
+        stats["ja_asr.jsut-basic5000"] = dataset_statistics("ja_asr.jsut-basic5000")
+    if "common_voice_8_0" not in stats:
+        stats["common_voice_8_0"] = dataset_statistics("common_voice_8_0")
+    # if "reazonspeech.tiny" not in stats:
+    #     stats["reazonspeech.tiny"] = dataset_statistics(data_type="tiny")
+    # if "reazonspeech.small" not in stats:
+    #     stats["reazonspeech.small"] = dataset_statistics(data_type="small")
+    # if "reazonspeech.medium" not in stats:
+    #     stats["reazonspeech.medium"] = dataset_statistics(data_type="medium")
+    # if "reazonspeech.large" in stats:
+    #     stats["reazonspeech.large"] = dataset_statistics(data_type="large")
+    # if "reazonspeech.all" in stats:
+    #     stats["reazonspeech.all"] = dataset_statistics(data_type="all")
+
+    with open("data_statistics.json", "w") as f:
+        json.dump(stats, f)
 
 
 
