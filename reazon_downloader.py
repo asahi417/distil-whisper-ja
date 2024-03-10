f384740d46fd4ffaeb2acc21a249fe797a09f63f
asahi417 <asahi1992ushio@gmail.com>
2024-03-10 15:51:56 +0900

    refactor downloader

--- reazon_downloader.py
+++ reazon_downloader.py
@@ -5,44 +5,44 @@
 import argparse
 import os
 import urllib.request
-from time import time
-from os.path import expanduser
+from multiprocessing import Pool
+from tqdm import tqdm
+
+BASE_URL = "https://reazonspeech.s3.abci.ai/"
+DATASET = {
+    "tiny": {"tsv": 'v2-tsv/tiny.tsv', "audio": "v2/{:03x}.tar", "nfiles": 1},
+    "small": {"tsv": 'v2-tsv/small.tsv', "audio": "v2/{:03x}.tar", "nfiles": 12},
+    "medium": {"tsv": 'v2-tsv/medium.tsv', "audio": "v2/{:03x}.tar", "nfiles": 116},
+    "large": {"tsv": 'v2-tsv/large.tsv', "audio": "v2/{:03x}.tar", "nfiles": 579},
+    "all": {"tsv": 'v2-tsv/all.tsv', "audio": "v2/{:03x}.tar", "nfiles": 4096}
+}
+
+
+def dl(url, target_file):
+    try:
+        urllib.request.urlretrieve(url, target_file)
+        return True
+    except Exception as e:
+        print(f"network error during downloading {target_file}")
+        if os.path.exists(target_file):
+            os.remove(target_file)
+        return False
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='Download ReazonSpeech locally.')
-    parser.add_argument('--target', default="tiny", help="tiny/small/medium/large/all", type=str)
+    parser.add_argument('-t', '--target', default="tiny", help="tiny/small/medium/large/all", type=str)
+    parser.add_argument('-p', '--pool', default=500, help="thread pool", type=int)
     arg = parser.parse_args()
-    target = arg.target
-
-    base_url = "https://reazonspeech.s3.abci.ai/"
-    dataset = {
-        "tiny": {"tsv": 'v2-tsv/tiny.tsv', "audio": "v2/{:03x}.tar", "nfiles": 1},
-        "small": {"tsv": 'v2-tsv/small.tsv', "audio": "v2/{:03x}.tar", "nfiles": 12},
-        "medium": {"tsv": 'v2-tsv/medium.tsv', "audio": "v2/{:03x}.tar", "nfiles": 116},
-        "large": {"tsv": 'v2-tsv/large.tsv', "audio": "v2/{:03x}.tar", "nfiles": 579},
-        "all": {"tsv": 'v2-tsv/all.tsv', "audio": "v2/{:03x}.tar", "nfiles": 4096}
-    }
-
-    urls = [base_url + dataset[target]["audio"].format(idx) for idx in range(dataset[target]["nfiles"])]
-    urls.append(base_url + dataset[target]["tsv"])
-    target_dir = f"{expanduser('~')}/.cache/reazon_manual_download/{target}"
+    target_dir = f"{os.path.expanduser('~')}/.cache/reazon_manual_download/{arg.target}"
     os.makedirs(target_dir, exist_ok=True)
-    start_total = time()
-    for n, url in enumerate(urls):
-        start = time()
-        print(f"{n + 1}/{len(urls)}: {url}")
-        target_file = f"{target_dir}/{target}.{os.path.basename(url)}"
-        if os.path.exists(target_file):
-            continue
-        try:
-            urllib.request.urlretrieve(url, target_file)
-            print(f"- tmp: {time() - start} sec")
-            print(f"- total: {time() - start_total} sec")
-        except Exception as e:
-            print(f"network error during downloading {target_file}")
-            if os.path.exists(target_file):
-                os.remove(target_file)
-            exit()
-    print(f"total: {time()-start_total} sec")
+
+    # all urls to download
+    urls = [BASE_URL + DATASET[arg.target]["audio"].format(idx) for idx in range(DATASET[arg.target]["nfiles"])]
+    urls.append(BASE_URL + DATASET[arg.target]["tsv"])
+    urls = [i for i in urls if not os.path.exists(f"{target_dir}/{arg.target}.{os.path.basename(i)}")]
+    filenames = [f"{target_dir}/{arg.target}.{os.path.basename(i)}" for i in urls]
+
+    # start downloader
+    pool = Pool(arg.pool)
+    pool.starmap_async(dl, tqdm(zip(urls, filenames), total=len(filenames)))
