9ee5ed8dc91e8d2361f516d71ef583d36ba6df68
asahi417 <asahi1992ushio@gmail.com>
2024-03-10 14:48:47 +0900

    refactor

--- reazon_downloader.py
+++ reazon_downloader.py
@@ -8,38 +8,41 @@ import urllib.request
 from time import time
 from os.path import expanduser
 
-parser = argparse.ArgumentParser(description='Download ReazonSpeech locally.')
-parser.add_argument('--target', default="tiny", help="tiny/small/medium/large/all", type=str)
-arg = parser.parse_args()
-target = arg.target
 
-base_url = "https://reazonspeech.s3.abci.ai/"
-dataset = {
-    "tiny": {"tsv": 'v2-tsv/tiny.tsv',   "audio": "v2/{:03x}.tar", "nfiles": 1},
-    "small": {"tsv": 'v2-tsv/small.tsv',  "audio": "v2/{:03x}.tar", "nfiles": 12},
-    "medium": {"tsv": 'v2-tsv/medium.tsv', "audio": "v2/{:03x}.tar", "nfiles": 116},
-    "large": {"tsv": 'v2-tsv/large.tsv',  "audio": "v2/{:03x}.tar", "nfiles": 579},
-    "all": {"tsv": 'v2-tsv/all.tsv',    "audio": "v2/{:03x}.tar", "nfiles": 4096}
-}
+if __name__ == '__main__':
 
-urls = [base_url + dataset[target]["audio"].format(idx) for idx in range(dataset[target]["nfiles"])]
-urls.append(base_url + dataset[target]["tsv"])
-target_dir = f"{expanduser('~')}/.cache/reazon_manual_download/{target}"
-os.makedirs(target_dir, exist_ok=True)
-start_total = time()
-for n, url in enumerate(urls):
-    start = time()
-    print(f"{n + 1}/{len(urls)}: {url}")
-    target_file = f"{target_dir}/{target}.{os.path.basename(url)}"
-    if os.path.exists(target_file):
-        continue
-    try:
-        urllib.request.urlretrieve(url, target_file)
-        print(f"- tmp: {time() - start} sec")
-        print(f"- total: {time() - start_total} sec")
-    except Exception as e:
-        print(f"network error during downloading {target_file}")
+    parser = argparse.ArgumentParser(description='Download ReazonSpeech locally.')
+    parser.add_argument('--target', default="tiny", help="tiny/small/medium/large/all", type=str)
+    arg = parser.parse_args()
+    target = arg.target
+
+    base_url = "https://reazonspeech.s3.abci.ai/"
+    dataset = {
+        "tiny": {"tsv": 'v2-tsv/tiny.tsv', "audio": "v2/{:03x}.tar", "nfiles": 1},
+        "small": {"tsv": 'v2-tsv/small.tsv', "audio": "v2/{:03x}.tar", "nfiles": 12},
+        "medium": {"tsv": 'v2-tsv/medium.tsv', "audio": "v2/{:03x}.tar", "nfiles": 116},
+        "large": {"tsv": 'v2-tsv/large.tsv', "audio": "v2/{:03x}.tar", "nfiles": 579},
+        "all": {"tsv": 'v2-tsv/all.tsv', "audio": "v2/{:03x}.tar", "nfiles": 4096}
+    }
+
+    urls = [base_url + dataset[target]["audio"].format(idx) for idx in range(dataset[target]["nfiles"])]
+    urls.append(base_url + dataset[target]["tsv"])
+    target_dir = f"{expanduser('~')}/.cache/reazon_manual_download/{target}"
+    os.makedirs(target_dir, exist_ok=True)
+    start_total = time()
+    for n, url in enumerate(urls):
+        start = time()
+        print(f"{n + 1}/{len(urls)}: {url}")
+        target_file = f"{target_dir}/{target}.{os.path.basename(url)}"
         if os.path.exists(target_file):
-            os.remove(target_file)
-        exit()
-print(f"total: {time()-start_total} sec")
+            continue
+        try:
+            urllib.request.urlretrieve(url, target_file)
+            print(f"- tmp: {time() - start} sec")
+            print(f"- total: {time() - start_total} sec")
+        except Exception as e:
+            print(f"network error during downloading {target_file}")
+            if os.path.exists(target_file):
+                os.remove(target_file)
+            exit()
+    print(f"total: {time()-start_total} sec")
